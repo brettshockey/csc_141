585cf887f73e807a1ed505c83fe03c31ccecad76
brett_shockey <brett.shockey001@albright.edu>
2025-09-04 12:02:30 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,15 @@
+'''
+
+I am looking through python.org
+
+I am interested in Open Source Coding
+
+I am interested in Audio/Video within coding 
+
+I learned that python is free
+
+Python community is based on mutual respect, tolerance, and encouragement
+
+The mission of python software is to promote, protect, andadvance the phython language to everyone
+
+'''
