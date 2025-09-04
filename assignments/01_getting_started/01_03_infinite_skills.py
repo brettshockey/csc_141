a3e3a1bf7baa121ada76dc73ce71afd042fd1af9
brett_shockey <brett.shockey001@albright.edu>
2025-09-04 12:13:02 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,17 @@
+'''
+
+I want to create a video game
+
+A video game related to Superheros and aliens
+
+I wanna learn to incorperate music into coding
+
+Develop a chatbot using natural language processing 
+
+Make a stock market prediction app 
+
+I wanna remake the flappy bird game
+
+I Learn how to code in different languages.
+
+'''
