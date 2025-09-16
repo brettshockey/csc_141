f8458372fde19c4f8684cc95c4ecfe57b829c039
brett_shockey <brett.shockey001@albright.edu>
2025-09-16 12:05:13 -0400

    Add files via upload

---
+++ assignments/04_working_with_lists/04_05_summing_a_million.py
@@ -0,0 +1,17 @@
+# Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. Also, use the sum() function to see how quickly Python can add a million numbers.
+
+numbers = list(range(1, 1000001))
+
+
+print(min(numbers))
+print(max(numbers))
+
+# Make a list of the numbers from one to one million, and then use min() and max() to make sure your list actually starts at one and ends at one million. Also, use the sum() function to see how quickly Python can add a million numbers.
+
+numbers = list(range(1, 1000001))
+
+
+print(min(numbers))
+print(max(numbers))
+8147269
+print(sum(numbers))
